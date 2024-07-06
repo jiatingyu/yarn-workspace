1aee1278d715bbaa46b545a16e6a5e4d00b3b84a
jiatingyu <jiatingyu94@gmail.com>
2024-07-06 09:40:23 +0800

    lerna

---
+++ packages/v-cart/index.js
@@ -0,0 +1,4 @@
+// import {baseInfo} from 'v-user'
+let {baseInfo} = require('v-user')
+
+console.log(JSON.stringify(baseInfo));
---
+++ packages/v-user/index.js
@@ -0,0 +1,7 @@
+const baseInfo = {
+    addr: "成都市",
+    age: 20,
+    name: "张三",
+  };
+  
+  module.exports = {baseInfo}
