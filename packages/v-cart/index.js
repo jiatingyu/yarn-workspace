552c3897c3e1acda3dbbcb141d6547564d17bf4b
jiatingyu <jiatingyu94@gmail.com>
2024-07-06 10:17:40 +0800

    切换包名

--- packages/v-cart/index.js
+++
@@ -1,4 +0,0 @@
-// import {baseInfo} from 'v-user'
-let {baseInfo} = require('v-user')
-
-console.log(JSON.stringify(baseInfo));
---
+++ packages/ziyu-cart/index.js
@@ -0,0 +1,3 @@
+let {baseInfo} = require('ziyu-user')
+
+console.log(JSON.stringify(baseInfo));
